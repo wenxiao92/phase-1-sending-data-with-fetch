1ece15af3a7bc7c4dccd799779cec64000dd4f30
Wen Xiao <wennstyle@gmail.com>
2021-10-19 23:16:10 -0400

    commit tree 741f288fe9fc0d6b31921f5c7acd9723dc9120e8

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Add your code here
+function submitData(nameInput, emailInput) {
+    let formData = {};
+    formData.name = `${nameInput}`
+    formData.email = `${emailInput}`
+
+    const configureData = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+
+    return fetch("http://localhost:3000/users", configureData)
+    .then(function (response) {
+    //  console.log(response)
+        return response.json();
+    })
+    .then(function (object) {
+    //    console.log(object)
+        document.body.innerHTML = object["id"]
+    })
+    .catch(function (error) {
+        document.body.innerHTML = error
+    })
+
+}
+
+
+      
+//   fetch("http://localhost:3000/users", configureData)
+//     .then(function (response) {
+//       return response.json();
+//     })
+//     .then(function (object) {
+//       console.log(object);
+//     });
+//     .catch(function (error) {
+//         alert("Invalid name or email");
+//         console.log(error.message);
+//     });
